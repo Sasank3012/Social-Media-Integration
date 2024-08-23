087f27c52316e64f52831afdab4877d63a862923
Sasank3012 <158005498+Sasank3012@users.noreply.github.com>
2024-08-23 13:37:37 -0500

    Update twitter.py

--- twitter.py
+++ twitter.py
@@ -1,11 +1,5 @@
 import tweepy
 
-api_key = "TCBON2h9x5NeFKXqCgRIYQJ1P"
-api_secret = "87PIbBgDU2TwKkyu6zaVIo9SkVVnY5CykXYCP1Mvn7waeXtRYW"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAIOuqgEAAAAA7GlMD852PT3bBKDgfAow5qLQ31M%3DTnrvBHHAf0Lk8NkeBliIfeKBNsvyNx2cd6Y8jl77eLmjgwKU3y"
-access_token = "2317347446-MCdZzqRgccFaokBjo3Ln3kN2UxjQkgcifU1gqlJ"
-access_token_secret = "ykNL42NIgoP3lc3BHr8mNhSCka9YVqWNA6ZJ639Ly3qmk"
-
 client = tweepy.Client(bearer_token,api_key,api_secret,access_token,access_token_secret)
 auth = tweepy.OAuth1UserHandler(api_key,api_secret,access_token,access_token_secret)
 api = tweepy.API(auth)
