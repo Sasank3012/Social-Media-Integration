e44b568a91ab721bd4b0d5e762cc48a035b89ef6
Sasank3012 <158005498+Sasank3012@users.noreply.github.com>
2024-08-23 13:40:02 -0500

    Update tweets2.py

--- tweets2.py
+++ tweets2.py
@@ -1,10 +1,10 @@
 import tweepy
 from tweepy import TweepyException
-api_key = "TCBON2h9x5NeFKXqCgRIYQJ1P"
-api_secret = "87PIbBgDU2TwKkyu6zaVIo9SkVVnY5CykXYCP1Mvn7waeXtRYW"
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAIOuqgEAAAAA7GlMD852PT3bBKDgfAow5qLQ31M%3DTnrvBHHAf0Lk8NkeBliIfeKBNsvyNx2cd6Y8jl77eLmjgwKU3y"
-access_token = "2317347446-MCdZzqRgccFaokBjo3Ln3kN2UxjQkgcifU1gqlJ"
-access_token_secret = "ykNL42NIgoP3lc3BHr8mNhSCka9YVqWNA6ZJ639Ly3qmk"
+api_key = ""
+api_secret = ""
+bearer_token = "%"
+access_token = "-"
+access_token_secret = ""
 
 
 def create_api():
